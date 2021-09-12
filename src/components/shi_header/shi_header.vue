d6ce4c6a8c835898e75ae42604316486b96ddff2
wangning <wangning@chunyu.me>
2021-09-12 11:38:56 +0800

    test

--- src/components/shi_header/shi_header.vue
+++ src/components/shi_header/shi_header.vue
@@ -1,29 +1,41 @@
+<!--
+ * @Author: wn
+ * @Date: 2021-09-12 11:37:21
+ * @LastEditTime: 2021-09-12 11:38:45
+ * @LastEditors: wn
+ * @Description: 
+-->
 <template>
-  <header class='shi_header'>
+  <header class="shi_header">
     <i class="shi_goBack iconfont icon-arrow3"></i>
     <slot name="shi_title"></slot>
     <i class="shi_seacher iconfont icon-search"></i>
   </header>
 </template>
 
-<script>
-export default {
 
-}
+
+
+<script>
+export default {};
 </script>
 
 <style lang="stylus" scoped>
-.shi_header
-  height 50px
-  background #f2f2f2
-  color: #666
-  display flex
-  align-items center
-  justify-content space-between
-  padding 0 10px
-  .shi_goBack
-    font-size 26px 
-  .shi_seacher
-    font-size 26px
+.shi_header {
+  height: 50px;
+  background: #f2f2f2;
+  color: #666;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
 
+  .shi_goBack {
+    font-size: 26px;
+  }
+
+  .shi_seacher {
+    font-size: 26pxheight 50pxbackground #f2f2f2 color;
+  }
+}
 </style>
